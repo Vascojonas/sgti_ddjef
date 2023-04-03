a5ff4dab7ebea52b3a0bd2ab725bc80095027290
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2023-04-03 18:25:57 +0200

    first commit

---
+++ src/test/java/com/ddjef/DdjefTransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddjef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdjefTransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
