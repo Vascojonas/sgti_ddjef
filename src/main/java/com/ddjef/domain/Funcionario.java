6e314af8542575656679bf4d0525a56353bc096f
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2023-04-04 12:32:21 +0200

    bus config

---
+++ src/main/java/com/ddjef/domain/Funcionario.java
@@ -0,0 +1,33 @@
+package com.ddjef.domain;
+
+import jakarta.persistence.*;
+
+
+//@Entity
+@SuppressWarnings("serial")
+
+@Table(name = "FUNCIONARIOS")
+
+public class Funcionario extends AbstractEntity<Long> {
+	
+	@Column(name ="nome", length=60)
+	private String nome;
+	
+	@Column(name ="apelido", length=60)
+	private String apelido;
+	
+	@Column(name ="telefone", length=20)
+	private String telefone;
+	
+	@Column(name ="email", length=20)
+	private String email;
+	
+	@Column(name ="tipo_doc", length=20)
+	private String tipo_doc;
+	
+	@Column(name ="numero_doc", length=60)
+	private String numero_doc;
+	
+	@Column(name ="cargo", length=60)
+	private String cargo;
+}
